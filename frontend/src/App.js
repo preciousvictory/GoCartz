b2a165e9bbdb67fd17df6d67124051bdb386609d
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-09-02 19:57:36 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { useState } from 'react';
-import './App.css';
 import Nav from './components/Nav';
+import { Nav2 } from './components/Nav';
 
 function App() {
   let [counter, setCounter] = useState(0);
   return (
     <div className="App">
       <Nav />
+      <Nav2 />
       <button onClick={() => setCounter(counter + 1)}>Click me</button>
     </div>
   );
--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,23 +1,101 @@
 import React from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar, Container, Button, Form, NavDropdown } from "react-bootstrap";
+import "../css/Nav.css";
 
 const Nav = () => {
   return (
     <Navbar className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src="/img/logo.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{" "}
-          React Bootstrap
+        <Navbar.Brand href="#home" className="logo">
+          <span style={{ color: "white" }}>Go</span>
+          <span style={{ color: "#F88A24" }}>Cartz</span>
         </Navbar.Brand>
       </Container>
     </Navbar>
   );
 };
 
+export function Nav2() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button className='search'>Search</Button>
+        </Form>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <NavDropdown title="WOMEN" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown title="MEN" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown title="GADGET" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown title="GROCERIES" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown title="KIDS" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
 export default Nav;
+// #F88A24
+// black - 1F1F25
